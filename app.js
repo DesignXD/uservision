118758779c36d503b29250a1cdec3cafb3e34bac
DesignXD <168326089+DesignXD@users.noreply.github.com>
2026-04-28 23:57:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://uservision-api-abdfdya4b2e3brcr.switzerlandnorth-01.azurewebsites.net/api";
+const API_BASE_URL = "https://uservision-api-adbfdya4b2e3brcr.switzerlandnorth-01.azurewebsites.net/api";
 
 let mediaItems = [];
 
